8acbd7889a6876c4d28d7dd735c59f7cbc839817
juliane <jcasier@protonmail.com>
2023-06-30 14:43:39 +0200

    adding static content

--- app/page/mentions-legales/page.tsx
+++ app/page/mentions-legales/page.tsx
@@ -1,8 +1,26 @@
+import styles from '@styles/modules/StaticLayout.module.scss';
 export default function MentionsLegales() {
   return (
     <>
-      <h1>Mentions légales</h1>
-      <p>Here it is</p>
+      <div className='titleWrapper'>
+        <h1>Mentions légales</h1>
+      </div>
+      <div className={styles.articleWrapper}>
+        <div className={styles.articleDecoration}>
+          <article>
+            <h2>Propriété intellectuelle</h2>
+            <p>Blabla</p>
+          </article>
+        </div>
+      </div>
+      <div className={styles.articleWrapper}>
+        <div className={styles.articleDecoration}>
+          <article>
+            <h2>Données à caractères personnelles</h2>
+            <p>Aucune donnée personnelle n&apos;est collectée pour ce site.</p>
+          </article>
+        </div>
+      </div>
     </>
   );
 }
